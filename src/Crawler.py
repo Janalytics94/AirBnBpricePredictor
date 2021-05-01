61beaee7f112b16b2f004af0ede6d2dcefe383f8
Jana Vihs <jana.vihs@doc-cirrus.com>
2021-05-01 19:43:14 +0000

    test

--- src/Crawler.py
+++ src/Crawler.py
@@ -4,7 +4,9 @@ import urllib.request
 import os 
 import sys 
 import time
+
 from clize import run
+from urllib.error import HTTPError
 
 def crawl_images(source, target):
     ''' Crawls all the images provided from the url and safes them into a folder'''
@@ -12,13 +14,15 @@ def crawl_images(source, target):
     path = os.path.join(source + 'train.csv') 
     train = pd.read_csv(path, index_col='listing_id')
     # get picture url
-    _pUrl_list = train.picture_url.unique().tolist()
-    filenames = train.index.unique().tolist()
+    pictures = train.picture_url
     # start crawling
     start = time.time()
-    for url in _pUrl_list:
-        for filename in filenames:
-            urllib.request.urlretrieve(url, target + filename + '.png')
+    for i in range(0,len(pictures)):
+        try:
+            urllib.request.urlretrieve(pictures.iloc[i], target + pictures.index[i] + '.png')
+        except HTTPError as e:
+            print('Picture does not exist')
+            continue
     ende = time.time()
     print('{:5.3f}s'.format(ende-start))     
     
