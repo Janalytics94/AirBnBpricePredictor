4ab7bad2504cf6295ee515b2edc73fc0252583ca
Jana Vihs <jana.vihs@doc-cirrus.com>
2021-05-09 23:56:23 +0000

    added TOC, opencv DockerImage and changed Yelpcrawler

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM jjanzic/docker-python3-opencv
 
 WORKDIR /root/
 
-COPY requirements.txt requirements.txt
+COPY requirements.txt /root/requirements.txt
 
 RUN apt-get update && \
+    apt-get install -y git &&\
     apt-get upgrade -y && \
-    apt-get install -y git \
     apt-get install ffmpeg libsm6 libxext6  -y
 #RUN pip3 install -r requirements.txt 
--- src/ingest/YelpCrawler.py
+++ src/ingest/YelpCrawler.py
@@ -5,59 +5,53 @@ import json
 import requests
 from time import sleep
 
-from clize import run
+api_key = 'dJ2fEA6y23SK0QhGgIweg0eOimGYx9IPTOeKg58GtUOMhSFN9qXJ82Q0WvNDSD_iiAg1CRUubPGa797eREBDQQpejqgQQVXj3R1qOSU2ARMEWduAWOfGOultbdqTYHYx'
+# We check the Endpoint business/ Get Method API
+url = 'https://api.yelp.com/v3/businesses/search'
 
-def crawl_yelp(target):
-    '''Crawls RestAPI of Yelp to get additional data for Neighbour Hoods'''
-    api_key = 'dJ2fEA6y23SK0QhGgIweg0eOimGYx9IPTOeKg58GtUOMhSFN9qXJ82Q0WvNDSD_iiAg1CRUubPGa797eREBDQQpejqgQQVXj3R1qOSU2ARMEWduAWOfGOultbdqTYHYx'
-    # We check the Endpoint business/ Get Method API
-    url = 'https://api.yelp.com/v3/businesses/search'
-   
-     # what do we want 
-    price = []
-    rating = []
-    coordinates = [] # might be useful later
-    zip_codes = []
+    # what do we want 
+price = []
+rating = []
+coordinates = [] # might be useful later
+zipcodes = []
 
-    headers = {'Authorization': 'Bearer %s' % api_key}
+headers = {'Authorization': 'Bearer %s' % api_key}
 
-    offset = 0
-     
-    ## loop to iterate over 200 pages of 500 businesses each = 100000 businesses in London 
-    while offset <=1:
-        try:
-            params={'term':'Restaurants', 'location': 'london', 'limit': 50, 'offset': offset}
-            req = requests.get(url, params=params, headers=headers)
-            parsed = json.loads(req.text)
-        except requests.exceptions.ConnectionError as e:
-            print('Too Many Requests, let me sleep for 10 seconds...')
-            sleep(10)
-            continue
-        # KANN MAN NOCH SCHICKER MACHEN
-        n = 0
-        while n <= 50:
-            try:
-                price_data = parsed["businesses"][n]['price']
-                ratings_data = parsed["businesses"][n]['rating']
-                zipcode_data = parsed["businesses"][n]["location"]["zip_code"]
-                coordinates_data = parsed['businesses'][n]['coordinates']
-                
-                rating.append(ratings_data)
-                zipcode.append(zipcode_data)
-                prices.append(price_data)
-                coordinates.append(coordinates_data)
-                
-            except:
-                ## some of the data gathered are not going to have the necessary information
-                ## so we skip those 
-                pass
-        offset += 1
+offset = 0
     
-    #Create dataframe of crawled data
-    yelp_df = pd.DataFrame({'rating': rating, 'zipcodes': zipcode, 'prices': prices, 'coordinates': coordinates}) 
-    yelp_df.to_csv(target)
+## loop to iterate over 200 pages of 50 businesses each = 10000 businesses in London 
+while offset <=200:
+    try:
+        params={'term':'Restaurants', 'location': 'london', 'limit': 50, 'offset': offset}
+        req = requests.get(url, params=params, headers=headers)
+        parsed = json.loads(req.text)
+    except requests.exceptions.ConnectionError as e:
+        print('Too Many Requests, let me sleep for 10 seconds...')
+        sleep(10)
+        continue
+    # KANN MAN NOCH SCHICKER MACHEN
+    n = 0
+    while n <= 50:
+        try:
+            price_data = parsed["businesses"][n]['price']
+            ratings_data = parsed["businesses"][n]['rating']
+            zipcode_data = parsed["businesses"][n]["location"]["zip_code"]
+            coordinates_data = parsed['businesses'][n]['coordinates']
+            
+            rating.append(ratings_data)
+            zipcodes.append(zipcode_data)
+            price.append(price_data)
+            coordinates.append(coordinates_data)
+            
+        except:
+            ## some of the data gathered are not going to have the necessary information
+            ## so we skip those 
+            pass
+    offset += 1
 
-    return
+#Create dataframe of crawled data
+a = {'rating': rating, 'zipcodes': zipcodes, 'prices': price, 'coordinates': coordinates}
+df = pd.DataFrame.from_dict(a, orient='index')
+df = df.transpose()
+df.to_csv('data/yelp.csv')
 
-if __name__ == '__main__':
-    run(crawl_yelp)
