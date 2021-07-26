8ea01c0696f10fd10d95949d622a9e13b4ce6334
Jana Vihs <jana.vihs@doc-cirrus.com>
2021-07-26 10:35:16 +0000

    WIP

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
+import os
+import xgboost
 from xgboost import XGBRegressor
+from clize import run
 
-class AirbnbPricePredictor():
+def predict(listing_id):
     ''' Holds the final method to predict prices of Airbnb's in London'''
-
-    def __init__(self):
-        model = XGBRegressor()
-        model.load_model('../data/models/xgb.json')
-        
-    def predict(self, listing_id): #'0FEMC4VA5U'
-        predictions = model.predict(X_test, ntree_limit=model.best_ntree_limit)
-        predictions = pd.Series(predictions.ravel(), index=test.index, name='price')
-        predictions = predictions.apply(lambda x: np.exp(x))
-        price = predictions.filter(like=listing_id)[0]
-        
-        return print('The predicted price of this Airbnb is: ' + str(round(price,2)) + " £.")
+    listing_id = str(listing_id)
+    model = XGBRegressor()
+    model.load_model(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "data/models/xgb.json"))
+    test = pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "data/canonical/test/test.csv"), index_col='listing_id')
+    predictions = model.predict(test, ntree_limit=model.best_ntree_limit)
+    predictions = pd.Series(predictions.ravel(), index=test.index, name='price')
+    predictions = predictions.apply(lambda x: np.exp(x))
+    price = predictions.filter(like=listing_id)[0]
+    
+    return print('The predicted price of this Airbnb is: ' + str(round(price,2)) + " £.")
 
 
 if __name__ == "__main__":
-    airbnb = AirbnbPricePredictor()
-    airbnb.predict('0FEMC4VA5U')
+    run(predict)
